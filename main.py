c45e923566b210f5d3e46dd58210dcfacc3db5c7
Chayka1 <106668633+Chayka1@users.noreply.github.com>
2023-01-11 16:14:08 +0200

    1

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
+import random
+
+
+def test_1():
+    class Coin:
+        def __init__(self):
+            self.__sideup = 'Орел'
+
+        def toss(self):
+            if random.randint(0, 1) == 0:
+                self.__sideup = 'Орел'
+            else:
+                self.__sideup = 'Решка'
+
+        def get_sideup(self):
+            return self.__sideup
+
+        def __str__(self):
+            return 'Подбрасываем...'
+
+    my_coin = Coin()
+    my_coin.toss()
+    print(my_coin)
+    print(my_coin.get_sideup())
+
+
 def task_1_1():
-    pass
+    class My:
+        def __init__(self):
+            self.__my_car = '10'
+
+        def go(self):
+            return self.__my_car
 
 
 if __name__ == '__main__':
-    pass
-
+    task_1_1()
